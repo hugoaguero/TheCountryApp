1c8347d58dfc92ff15f700b2107d5a6430e129f9
Hugo Aguero Arrieta <huguitoaguero@gmail.com>
2021-10-23 06:27:48 -0300

    Add: Shared and Countries module with its components and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountriesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
